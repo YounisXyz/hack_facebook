d7be8f76459fbd65c6e0d817698ea5eb625bb897
XyzCoder <106374961+YounisXyz@users.noreply.github.com>
2024-03-15 23:21:58 +0500

    Update xyz.py

--- xyz.py
+++ xyz.py
@@ -1,7 +1,26 @@
-import os, sys
+import os,time
 os.system("git pull")
-try:
-    __import__("xyz").YOUNIS()
-except Exception as e:
-    exit(str(e))
+os.system("clear")
+print("\033[1;96mSubscribe Me On YouTube Channel For Keep Sporting and Updating 😍❤️‍🩹🤡 ")
+time.sleep(2)
+os.system("xdg-open https://www.youtube.com/@YounisXyz")
+os.system("clear") 
+print("""\033[0;97mhack_facebook feature has been added in XYZ offical tool
+please use XYZ Command!""")
+
+print("\033[1;93m XYZ installation Command 👇💀")
+
+print("""\033[1;92m
+cd
+clear
+rm -rf Xyz
+git clone https://github.com/YounisXyz/Xyz
+cd Xyz
+python Xyz.py""")
+
+print("\033[0;97m\n\nCopy This Command and Run")
+print("\033[0;97mContact to Admin on WhatsApp For any Error: +923404708884")
+exit("\033[1;92mThanks For Using YounisXyz Tools ❤️💀")
+ 
+
  
